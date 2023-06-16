6225e492d141f6cd251d54d3d2a3750df5938066
lgouvin <gouvin.lucy@gmail.com>
2023-06-15 22:40:52 -0400

    started psuedo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+function generatePassword(){
+  // 
+
+  
+
+  return password
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
